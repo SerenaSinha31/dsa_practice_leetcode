455568bfa44a0986f0c36468647823998577a83a
Serena Sinha <114794148+SerenaSinha31@users.noreply.github.com>
2025-06-21 21:31:09 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (41.80%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
-
-        for (int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-
-            // Skip if the number is already considered
-            if (num == first || num == second || num == third)
-                continue;
-
-            if (num > first) {
-                third = second;
-                second = first;
-                first = num;
-            } else if (num > second) {
-                third = second;
-                second = num;
-            } else if (num > third) {
-                third = num;
-            }
+        long first = LONG_MIN, second=LONG_MIN, third= LONG_MIN;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+                 if(nums[i]==first || nums[i]==second|| nums[i]==third)
+        continue;
+        if(nums[i]>first){
+            third=second;
+            second = first;
+            first= nums[i];
         }
-
-        // Return third if it exists, otherwise the largest number
-        return third == LONG_MIN ? first : third;
+         else if(nums[i]>second){
+            third=second;
+          
+            second= nums[i];
+        }
+          else if(nums[i]>third){
+            
+            third= nums[i];
+        }
+        }
+       
+      if(third==LONG_MIN)//if third mila hi nhi 
+       {
+        return int(first);
+       }
+       else{
+        return int(third);
+       }
     }
 };
