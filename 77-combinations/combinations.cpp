ebc9f0352941049d9b8a61f3b8fca4664f0bd93f
Serena Sinha <114794148+SerenaSinha31@users.noreply.github.com>
2025-07-28 22:26:35 +0530

    Time: 35 ms (87.89%) | Memory: 62.5 MB (80.00%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> current_combination;
+        backtrack(n, k, 1, current_combination, result);
+        return result;
+    }
+    
+private:
+    void backtrack(int n, int k, int start, vector<int>& current, vector<vector<int>>& result) {
+        if (current.size() == k) {
+            result.push_back(current);
+            return;
+        }
+        for (int i = start; i <= n; ++i) {
+            current.push_back(i);
+            backtrack(n, k, i + 1, current, result);
+            current.pop_back(); // backtrack
+        }
+    }
+};
